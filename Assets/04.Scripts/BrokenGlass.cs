9c859d56a3aac76fdfa6392e93ce461c9b19e88f
PyoMyeongchan <vyaudcks@gmail.com>
2025-07-09 11:59:10 +0900

    [Feature] 유리 파편 애니메이션 / Rokoko studio 설치

---
+++ Assets/04.Scripts/BrokenGlass.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System;
+using Random = UnityEngine.Random;
+
+public class BrokenGlass : MonoBehaviour
+{
+    [Header("Glass Settings")]
+    [SerializeField] private GameObject _originalGlass;
+    [SerializeField] private GameObject _brokenGlassPrefab;
+
+    [Header("Physics Settings")]
+    [SerializeField] private float _explosionForce = 5f;
+    [SerializeField] private float _spread = 2f;
+
+    private GameObject _brokenInstance;
+    [SerializeField] private Camera mainCamera;
+    
+    public void StartBreakSequence()
+    {
+        if (_originalGlass == null || _brokenGlassPrefab == null)
+        {
+            return;
+        }
+
+        _originalGlass.SetActive(false);
+        
+        Vector3 brokenGlassRotation = new Vector3(90f, 45f, 90f);
+
+        _brokenInstance = Instantiate(
+            _brokenGlassPrefab,
+            _originalGlass.transform.position,
+            Quaternion.Euler(brokenGlassRotation)
+        );
+
+    }
+
+    public void BrokenGlasses()
+    {
+        if (_brokenInstance == null)
+        {
+            return;
+        }
+        ExplodeShards();
+    }
+
+    private void ExplodeShards()
+    {
+        Rigidbody[] shardRigidbodies = _brokenInstance.transform.GetComponentsInChildren<Rigidbody>();
+
+        Vector3 forward = mainCamera.transform.forward;
+        
+        foreach (Rigidbody rb in shardRigidbodies)
+        {
+            Vector3 randomDir = (-forward + Random.insideUnitSphere * _spread).normalized;
+            rb.AddForce(randomDir * _explosionForce, ForceMode.Impulse);
+            Destroy(rb.gameObject, 1f);
+        }
+    }
+}
